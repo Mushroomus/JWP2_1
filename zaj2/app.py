754154a009af4e63643aacb9095faa0c041e4002
UL0258010 <UL0258010@edu.uni.lodz.pl>
2024-04-28 12:19:15 +0200

    added zad4 with flask

---
+++ zaj2/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Witaj w mojej aplikacji Flask!"
+
+@app.route('/about')
+def about():
+    return "Zaprogramowano przez Piotr."
+
+@app.route('/contact')
+def contact():
+    return "Email: email@example.com"
+
+if __name__ == '__main__':
+    app.run(debug=True)
